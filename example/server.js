b1b45428c7d16287933b39a45d1989a04ab2fb73
Dennis Mårtensson <dennismartensson@gmail.com>
2012-08-03 17:44:10 -0400

    added message if peerconnection is not supported

--- example/server.js
+++ example/server.js
@@ -4,6 +4,8 @@ var io = require('webrtc.io').listen(app);
 var colors = {};
 var servers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
+
+
 //When connectiong to nodejitsu
 //app.listen(80);
 
