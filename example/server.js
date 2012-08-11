04fd8124438cd6f3a421f224cb5db4f39a359345
Dennis Mårtensson <dennismartensson@gmail.com>
2012-08-11 17:19:40 -0400

    chance of api fic

--- example/server.js
+++ example/server.js
@@ -36,13 +36,15 @@ webRTC.rtc.on('chat_msg', function(data, socket) {
     var socketId = roomList[i];
 
     if (socketId !== socket.id) {
-      var soc = webRTC.rtc.getSocket(data.room, socketId);
+      var soc = webRTC.rtc.getSocket(socketId);
 
       if (soc) {
         soc.send(JSON.stringify({
           "eventName": "receive_chat_msg",
-          "messages": data.messages,
-          "color": data.color
+          "data": {
+            "messages": data.messages,
+            "color": data.color
+          }
         }), function(error) {
           if (error) {
             console.log(error);
