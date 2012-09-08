18eecfaa8e0f47075c2bda0810dc883ee696f6fd
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-09-08 20:54:28 +0200

    Removed useless chat code

--- example/server.js
+++ example/server.js
@@ -36,31 +36,3 @@ webRTC.rtc.on('send answer', function(rtc) {
 webRTC.rtc.on('disconnect', function(rtc) {
   //Client disconnect 
 });
-
-/*
-webRTC.rtc.on('chat_msg', function(data, socket) {
-  var roomList = webRTC.rtc.rooms[data.room] || [];
-
-  for (var i = 0; i < roomList.length; i++) {
-    var socketId = roomList[i];
-
-    if (socketId !== socket.id) {
-      var soc = webRTC.rtc.getSocket(socketId);
-
-      if (soc) {
-        soc.send(JSON.stringify({
-          "eventName": "receive_chat_msg",
-          "data": {
-            "messages": data.messages,
-            "color": data.color
-          }
-        }), function(error) {
-          if (error) {
-            console.log(error);
-          }
-        });
-      }
-    }
-  }
-});
-*/
