2c4fd93f2426601bfee9baeb24e0bb7fe25d1280
Daniel Kutik <daniel@chaltigaadi.com>
2013-02-06 18:19:41 +0530

    Small mistake: removed duplicate of 'server ='

--- example/server.js
+++ example/server.js
@@ -1,5 +1,5 @@
 var app = require('express')();
-var server = server = require('http').createServer(app);
+var server = require('http').createServer(app);
 var webRTC = require('webrtc.io').listen(server);
 
 server.listen(8000);
